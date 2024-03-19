8609ad8950435b277eab055529f92b8a04c0f753
hardik-pratap-singh <21bcs090@iiitdmj.ac.in>
2024-03-20 00:34:15 +0530

    Post database setup complete

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+# this is where we need to register our models so that they show up on the admin page
+
+admin.site.register(Post)
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
 
@@ -7,31 +8,31 @@ from django.shortcuts import render
 #     return HttpResponse("<h1>Hello World</h1>")
 
 
-posts = [
-    {
-        'author' : 'hps' , 
-        'title' : 'abc' , 
-        'content' : 'lorem34',
-        'date_posted' : 'August 27, 2018'
-    },
-    {
-        'author' : 'hpssingh' , 
-        'title' : 'def' , 
-        'content' : 'lorem2394',
-        'date_posted' : 'August 15, 2018'
-    },
-    {
-        'author' : 'hps12345' , 
-        'title' : 'abcdefg' , 
-        'content' : 'bengal tiger reserver',
-        'date_posted' : 'August 27, 2020'
-    }
-]
+# posts = [
+#     {
+#         'author' : 'Sierra Elman' , 
+#         'title' : 'The Future of Poetry' , 
+#         'content' : 'Is AI Smarter Than an 8th Grader?',
+#         'date_posted' : 'Mar 12, 2024'
+#     },
+#     {
+#         'author' : 'Tim Berners-Lee' , 
+#         'title' : 'Marking the Web’s 35th Birthday: An Open Letter' , 
+#         'content' : 'Sir Tim Berners-Lee’s open letter to mark the occasion of the Web’s 35th Birthday.',
+#         'date_posted' : 'Mar 11, 2024'
+#     },
+#     {
+#         'author' : 'hps12345' , 
+#         'title' : 'Would Albert Einstein End Up in Academia in 2024?' , 
+#         'content' : 'Imagine that you are a young Albert Einstein in Gen Z, born 120 years after the legendary scientist, with a new concept for..',
+#         'date_posted' : 'Mar 6, 2024'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts' : posts 
+        'posts' : Post.objects.all() 
     }
     return render(request , 'blog/home.html' , context)
 
